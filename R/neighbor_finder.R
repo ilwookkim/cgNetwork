e3ce3d09a574e3af46d7068cbbcd0bd5fa95634e
Solatar <50340626+Solatar@users.noreply.github.com>
2021-02-15 17:02:55 +0100

    Update neighbor_finder.R
    added an error note if the gene of interest is not in the matrix. Before, the function would calculate first (which could take some time) and then give an error if the gene is not present. Now, this is checked before everything else.
    
    removed CDKNA1 as the default gene (if this is accidentally forgotten by the user it is better the function complains, than to spend time calculating something the user does not want)
    
    added some function description

--- R/neighbor_finder.R
+++ R/neighbor_finder.R
@@ -1,17 +1,24 @@
 #' neighbor_finder
 #'
+#' Imports:
+#' igraph
+#'
 #' This function allows the user to find neighbor genes network of interested gene.
 #' @param big_cor_matrix bigmatrix (bigmemory package) of Spearman's correlation coefficient from TCGA RNAseq countdata
-#' @param gene Interesting gene to find network. Defaults to "CDKN1A".
+#' @param gene string, gene of interest to find a network for.
 #' @param cor.cut.off cut off value of correlation coeffiecint to build network. Defaults to .39
 #' @param weight.cut.off cut off value of network weight to reduce edges. Defaults to .5
+#' @return character vector, genes that are considered neighbors of the gene of interest (including the gene of interest itself).
 #' @examples
 #' common_neighbor <- neighbor_finder(big_cor_matrix, gene="CDKN1A", cor_method = "spearman", cor.cut.off=.39, weight.cut.off=.5)
 #' @export
 
-neighbor_finder <- function(big_cor_matrix, gene="CDKN1A", cor.cut.off=.39, weight.cut.off=.5){
+neighbor_finder <- function(big_cor_matrix, gene, cor.cut.off=.39, weight.cut.off=.5){
+  #check if the gene is at all present. Otherwise there would be a calculation first (which might take long) and then the error.
+  if(!any(rownames(a1) %in% gene)) stop("Your gene of interest is not in the matrix.")
+  
   big_cor_matrix[which(big_cor_matrix[,] == 1)] <- 0
-  big_cor_matrix[which(abs(big_cor_matrix[,]) < cor.cut.off)] <- 0
+  big_cor_matrix[which(big_cor_matrix[,] < cor.cut.off)] <- 0
   net <- igraph::graph_from_adjacency_matrix(big_cor_matrix[,], mode='undirected', weighted = T, diag=F)
   net <- igraph::simplify(net, remove.multiple = T, remove.loops = T)
   net.sp <- igraph::delete_edges(net, igraph::E(net)[weight<weight.cut.off])
