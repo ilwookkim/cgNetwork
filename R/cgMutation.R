0904e6865c1dbe40adf10756e0300c499c14bc1e
Solatar <50340626+Solatar@users.noreply.github.com>
2021-02-15 17:26:34 +0100

    Create cgMutation.R

---
+++ R/cgMutation.R
@@ -0,0 +1,24 @@
+#' convenient shortcut to find mutations
+#'
+#' Imports:
+#' cgdsr,
+#' org.Hs.eg.db
+#'
+#' @param cgds object, output from the cgBase() function
+#' @param cancer_study_id string, the as given in the cgBase() printout
+#' @param genes genes string vector with genes of interest. Symbols, EntrezIds and EnsemblIds work. If nothing (NA) is provided, all genes in the GO database will be used. This will take some time, so if you only need a specific set of genes, specify it here.
+#' @param caselist_name string, should be chosen so that only samples with mutation data are used.
+#' @return data.frame, each row is a patient, each column is a gene
+#' @details
+#' @examples
+#' cgds <- cgBbase()
+#' mystudy <- "laml_tcga"
+#' myMutations <- cgData(cgds, mystudy, genes=c("FLT3","TP53"))
+#' @export
+cgMutation <- function(cgds, cancer_study_id, genes=NA, caselist_name="Samples with mutation data"){
+  outtable <- cgData(cgds=cgds, cancer_study_id=cancer_study_id, profile_name="Mutations", caselist_name=caselist_name, genes)
+  outtable <- apply(outtable, 2, function(x) ifelse(is.na(x), 0, 1))
+  colnames(outtable) <- paste0(colnames(outtable), "_mutation_status")
+  #==output==#
+  as.data.frame(outtable)
+}
