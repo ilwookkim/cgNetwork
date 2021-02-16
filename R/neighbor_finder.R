c074a958ffbc5fecfda812a0a5de8bc21810ec01
ilwookkim <67730977+ilwookkim@users.noreply.github.com>
2021-02-16 01:30:14 +0100

    #

--- R/neighbor_finder.R
+++ R/neighbor_finder.R
@@ -5,23 +5,29 @@
 #' bigmemory
 #'
 #' This function allows the user to find neighbor genes network of interested gene.
-#' @param big_cor_matrix bigmatrix (bigmemory package) of Spearman's correlation coefficient from TCGA RNAseq countdata
+#' @param countdata RNAseq countdata
 #' @param gene string, gene of interest to find a network for.
 #' @param cor.cut.off cut off value of correlation coeffiecint to build network. Defaults to .39
 #' @param weight.cut.off cut off value of network weight to reduce edges. Defaults to .5
+#' @param t Matrix Transpose
 #' @return character vector, genes that are considered neighbors of the gene of interest (including the gene of interest itself).
 #' @examples
 #' common_neighbor <- neighbor_finder(big_cor_matrix, gene="CDKN1A", cor_method = "spearman", cor.cut.off=.39, weight.cut.off=.5)
 #' @export
 
-neighbor_finder <- function(countdata, gene, cor.cut.off=.39, weight.cut.off=.5){
+neighbor_finder <- function(countdata, gene, cor.cut.off=.39, weight.cut.off=.5, t=TRUE){
+  if(t == TRUE){
+    countdata_t <- data.frame(t(countdata))
+  } else {
+    countdata_t <- data.frame(countdata)
+  }
   countdata_t <- data.frame(t(countdata))
   countdata_t <- countdata_t[, !sapply(countdata_t, function(x) { sd(x) == 0} )]
   big_cor_matrix <- bigmemory::as.big.matrix(cor(countdata_t, method = "spearman"))
-  
+
   #check if the gene is at all present. Otherwise there would be a calculation first (which might take long) and then the error.
   if(!any(rownames(a1) %in% gene)) stop("Your gene of interest is not in the matrix.")
-  
+
   big_cor_matrix[which(big_cor_matrix[,] == 1)] <- 0
   big_cor_matrix[which(big_cor_matrix[,] < cor.cut.off)] <- 0
   net <- igraph::graph_from_adjacency_matrix(big_cor_matrix[,], mode='undirected', weighted = T, diag=F)
